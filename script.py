eb702bd217769ab34bdca25d84cd0cc3066467d9
CatKnife <72387902+cintagram@users.noreply.github.com>
2024-03-30 09:14:45 +0900

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,7 @@
 import os
 
-#NOTICE!!!!!!!
-#Change Unit_Explanation_ko.csv in line 20 to your game version's resLocal format (dont include number)
-
 if __name__ == "__main__":
-	filenames=os.listdir("units")
+	filenames=os.listdir("resLocal")
 	filenamesnum=len(filenames)
 	i = 0
 	while i <= filenamesnum:
@@ -13,11 +10,27 @@ if __name__ == "__main__":
 			print("i is None")
 			break
 		else:
-			k = open(("units/"+filenames[i]), "r", encoding="utf-8").read()
+			if not "Unit_Explanation" in filenames[i]:
+				os.remove("resLocal/"+filenames[i])
+			else:
+				pass
+	
+			
+
+	filenames=os.listdir("resLocal")
+	filenamesnum=len(filenames)
+	i = 0
+	while i <= filenamesnum:
+		i += 1
+		if i == filenamesnum or filenames[i] == None:
+			print("i is None")
+			break
+		else:
+			k = open(("resLocal/"+filenames[i]), "r", encoding="utf-8").read()
 			p = k.split("\n")[0].split("|")[0]
 			if p == None or p == "":
 				p = "None"
-			whhe = (int(filenames[i].strip("Unit_Explanation_ko.csv")) - 1)
+			whhe = (int(filenames[i].strip("Unit_Explanation_.csv").strip("en").strip("ko").strip("tw").strip("ja")) - 1)
 			open("catdb.csv", "a+", encoding="utf-8").write((p+","+str(whhe)+"\n"))
 
 	print("finished")
