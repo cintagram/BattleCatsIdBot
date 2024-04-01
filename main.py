b31d7ff110ea20e1ec492eebe2322796d1977eef
CatKnife <72387902+cintagram@users.noreply.github.com>
2024-04-01 10:38:04 +0900

    main.py 업데이트

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import pandas as pd
 import csv
 from os import walk
 
+forbiddenwords = ["cats"] #ypu can add more
+
 class MyClient(discord.Client):
   async def on_ready(self):
     await self.wait_until_ready()
@@ -20,26 +22,29 @@ intents= discord.Intents.all()
 client = MyClient(intents=intents)
 tree = app_commands.CommandTree(client)
     
-@tree.command(name="search", description="bc catid search")
+@tree.command(name="냥코검색", description="냥코 아이디검색")
 async def CatSearchCommand(interaction: Interaction, usr_input: str):
-	catdb = open("catdb.csv", "r", encoding="utf-8").read().split("\n")
-	line_len = len(catdb)
-	ctx = []
-	for i in range(line_len-1):
-		catname = str(catdb[i]).split(",")[0]
-		catid = str(catdb[i]).split(",")[1]
-		if usr_input in catname:
-			ctx.append(str(catname)+": "+str(catid)+"\n")
-		else:
-			pass
-	if len(ctx) == 0:
-		embed_body = "No results"
+	if len(usr_input) <= 2 or usr_input in forbiddenwords:
+		embed_body = "Forbidden Words (Prevent Rate Limit & Spam).\nPlease search other words."
 	else:
-		embed_body = ""
-		for k in range(len(ctx)):
-			embed_body += ctx[k]
+		catdb = open("catdb.csv", "r", encoding="utf-8").read().split("\n")
+		line_len = len(catdb)
+		ctx = []
+		for i in range(line_len-1):
+			catname = str(catdb[i]).split(",")[0]
+			catid = str(catdb[i]).split(",")[1]
+			if usr_input in catname:
+				ctx.append(str(catname)+": "+str(catid)+"\n")
+			else:
+				pass
+		if len(ctx) == 0:
+			embed_body = "No Results"
+		else:
+			embed_body = ""
+			for k in range(len(ctx)):
+				embed_body += ctx[k]
 	embed = discord.Embed(title="Results", description=embed_body)
-	await interaction.response.send_message(embed=embed)
+	await interaction.response.send_message(ephemeral=True, embed=embed)
 			
 
 client.run("TOKEN HERE")
